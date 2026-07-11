86fe51a217a24bc17cc69da13fe3d33b671fa9f7
L Young <techavenida@gmail.com>
2026-07-11 10:11:56 -0700

    Add sitemap and robots.txt generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://connectorselection.com',
+  generateRobotsTxt: true,
+  trailingSlash: true,
+  outDir: './out',
+}
